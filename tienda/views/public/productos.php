76d713c2e3861e7756310812da1ccbedfc83e221
fabs1902 <fabiolamartinez190201@gmail.com>
2019-02-03 11:20:41 -0600

    Hey
    Aquí te va el ejemplo del profe

---
+++ tienda/views/public/productos.php
@@ -0,0 +1,10 @@
+<?php
+include "../../core/helpers/public_page.php";
+Public_page::header();
+?>
+<div class="progress">
+    <div class="indeterminate"></div>
+</div>
+<?php
+Public_page::footer();
+?>
