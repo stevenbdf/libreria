df61d359848a5b6aa305766a6c0da62bdb7bb144
Steven <stevenbdf@gmail.com>
2019-02-18 19:55:25 -0600

    finished edit modal, added functions to table rows

--- resources/js/index.js
+++ resources/js/index.js
@@ -9,20 +9,9 @@ $("#botoncito").click(function() {
     confirmButtonAriaLabel: 'Thumbs up, great!',
   })
 })
+/* alert para eliminar objetos */
 
-$("#botoncito").click(function() {
-  Swal.fire({
-    title: '<strong>TERMINOS Y CONDICIONES</strong>',
-    type: 'info',
-    html: '<p class="text-justify ml-3 mr-3">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using , making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like). </p>',
-    showCloseButton: true,
-    focusConfirm: true,
-    confirmButtonText: '<i class="fa fa-thumbs-up"></i> OK!',
-    confirmButtonAriaLabel: 'Thumbs up, great!',
-  })
-})
-
-function deleteAlert(){
+function deleteAlert(objeto='registro'){
   Swal.fire({
     title: '¿Estas seguro?',
     text: "No podras recuperar la información borrada",
@@ -36,12 +25,14 @@ function deleteAlert(){
     if (result.value) {
       Swal.fire(
         '¡Borrado!',
-        'El registro ha sido eliminado',
+        `El ${objeto} ha sido eliminado`,
         'success'
       )
     }
   })
 }
+
+/* alert para guardar objetos */
 
 function savedAlert(objeto='Registro'){
   Swal.fire(
@@ -51,6 +42,33 @@ function savedAlert(objeto='Registro'){
   )
 }
 
+/* alert para modificar objetos */
+
+function modifyAlert(objeto='Registro'){
+  Swal.fire(
+  '¡Correcto!',
+  `${objeto} modificado correctamente`,
+  'success'
+  )
+}
+
+function cleanSelectedRows(){
+  for (var i = 0; i < 11; i++) {
+      $(`tr#${i}`).removeClass('selectedRow');
+  }
+}
+
+function selectedRow(id){
+
+  if($(`tr#${id}`).hasClass('selectedRow')){
+    $(`tr#${id}`).removeClass('selectedRow');
+  }else{
+    cleanSelectedRows()
+    $(`tr#${id}`).addClass('selectedRow');
+  }
+}
+
+/* Optimizar esta funcion */
 
 $(document).ready(function() {
   $("#email").focus(function() {
--- views/dashboard/products.php
+++ views/dashboard/products.php
@@ -291,7 +291,7 @@
                       <i class="fas fa-pen"></i>
                     </a>
                   </div>
-                  <div class="icon icon-shape bg-danger text-white rounded-circle shadow ml-md-2 ml-lg-3 mt-md-2 mt-lg-0" data-toggle="tooltip" data-placement="top" title="Eliminar" onclick="deleteAlert()">
+                  <div class="icon icon-shape bg-danger text-white rounded-circle shadow ml-md-2 ml-lg-3 mt-md-2 mt-lg-0" data-toggle="tooltip" data-placement="top" title="Eliminar" onclick="deleteAlert('Libro')">
                     <a>
                       <i class="fas fa-trash-alt"></i>
                     </a>
@@ -300,7 +300,7 @@
               </div>
             </div>
             <div class="table-responsive">
-              <table class="table align-items-center table-dark table-flush">
+              <table class="table align-items-center table-dark table-flush" id="productos">
                 <thead class="thead-dark">
                   <tr>
                     <th scope="col">Titulo</th>
@@ -312,7 +312,7 @@
                   </tr>
                 </thead>
                 <tbody>
-                  <tr onclick="alert('me tocaste')">
+                  <tr id="1" onclick="selectedRow(1)">
                     <th scope="row">
                       <div class="media align-items-center" style="">
                         <span class="mb-0 text-sm">101 mensajes para decir te quiero para colorear y regalar</span>
@@ -334,7 +334,7 @@
 
                     </td>
                   </tr>
-                  <tr>
+                  <tr id="2" onclick="selectedRow(2)">
                     <th scope="row">
                       <div class="media align-items-center" style="">
                         <span class="mb-0 text-sm">101 mensajes para decir te quiero para colorear y regalar</span>
@@ -356,7 +356,7 @@
 
                     </td>
                   </tr>
-                  <tr>
+                  <tr id="3" onclick="selectedRow(3)">
                     <th scope="row">
                       <div class="media align-items-center" style="">
                         <span class="mb-0 text-sm">101 mensajes para decir te quiero para colorear y regalar</span>
@@ -378,7 +378,7 @@
 
                     </td>
                   </tr>
-                  <tr>
+                  <tr id="4" onclick="selectedRow(4)">
                     <th scope="row">
                       <div class="media align-items-center" style="">
                         <span class="mb-0 text-sm">101 mensajes para decir te quiero para colorear y regalar</span>
@@ -400,7 +400,7 @@
 
                     </td>
                   </tr>
-                  <tr>
+                  <tr id="5" onclick="selectedRow(5)">
                     <th scope="row">
                       <div class="media align-items-center" style="">
                         <span class="mb-0 text-sm">101 mensajes para decir te quiero para colorear y regalar</span>
@@ -422,7 +422,7 @@
 
                     </td>
                   </tr>
-                  <tr>
+                  <tr id="6" onclick="selectedRow(6)">
                     <th scope="row">
                       <div class="media align-items-center" style="">
                         <span class="mb-0 text-sm">101 mensajes para decir te quiero para colorear y regalar</span>
@@ -444,7 +444,7 @@
 
                     </td>
                   </tr>
-                  <tr>
+                  <tr id="7" onclick="selectedRow(7)">
                     <th scope="row">
                       <div class="media align-items-center" style="">
                         <span class="mb-0 text-sm">101 mensajes para decir te quiero para colorear y regalar</span>
@@ -466,7 +466,7 @@
 
                     </td>
                   </tr>
-                  <tr>
+                  <tr id="8" onclick="selectedRow(8)">
                     <th scope="row">
                       <div class="media align-items-center" style="">
                         <span class="mb-0 text-sm">101 mensajes para decir te quiero para colorear y regalar</span>
@@ -488,7 +488,7 @@
 
                     </td>
                   </tr>
-                  <tr>
+                  <tr id="9" onclick="selectedRow(9)">
                     <th scope="row">
                       <div class="media align-items-center" style="">
                         <span class="mb-0 text-sm">101 mensajes para decir te quiero para colorear y regalar</span>
@@ -510,7 +510,7 @@
 
                     </td>
                   </tr>
-                  <tr>
+                  <tr id="10" onclick="selectedRow(10)">
                     <th scope="row">
                       <div class="media align-items-center" style="">
                         <span class="mb-0 text-sm">101 mensajes para decir te quiero para colorear y regalar</span>
@@ -609,34 +609,34 @@
                       <label for="message-text" class="col-form-label">Idioma:</label>
                       <div class="input group dropdown d-block">
                         <a href="#" class="btn btn-default dropdown-toggle " data-toggle="dropdown" id="navbarDropdownMenuLink2">
-                            <img src="../../resources/img/flags/SV.png" /> ...
+                          <img src="../../resources/img/flags/SV.png" /> ...
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink2">
-                            <li>
-                                <a class="dropdown-item" href="#">
-                                  <img src="../../resources/img/flags/DE.png" /> Deutsch
-                                </a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="#">
-                                  <img src="../../resources/img/flags/GB.png" /> English(UK)
-                                </a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="#">
-                                  <img src="../../resources/img/flags/FR.png" /> Français
-                                </a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="#">
-                                  <img src="../../resources/img/flags/ES.png" /> Español
-                                </a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="#">
-                                  <img src="../../resources/img/flags/PT.png" /> Portuges
-                                </a>
-                            </li>
+                          <li>
+                            <a class="dropdown-item" href="#">
+                              <img src="../../resources/img/flags/DE.png" /> Deutsch
+                            </a>
+                          </li>
+                          <li>
+                            <a class="dropdown-item" href="#">
+                              <img src="../../resources/img/flags/GB.png" /> English(UK)
+                            </a>
+                          </li>
+                          <li>
+                            <a class="dropdown-item" href="#">
+                              <img src="../../resources/img/flags/FR.png" /> Français
+                            </a>
+                          </li>
+                          <li>
+                            <a class="dropdown-item" href="#">
+                              <img src="../../resources/img/flags/ES.png" /> Español
+                            </a>
+                          </li>
+                          <li>
+                            <a class="dropdown-item" href="#">
+                              <img src="../../resources/img/flags/PT.png" /> Portuges
+                            </a>
+                          </li>
                         </ul>
                       </div>
                     </div>
@@ -662,9 +662,13 @@
                   <textarea id="resena" class="form-control form-control-alternative" rows="5" placeholder="Escriba una breve descripción ..."></textarea>
 
                   <div class="row">
-                    <div class="col-6">
+                    <div class="col-3">
                       <label for="message-text" class="col-form-label">Precio:</label>
                       <input type="number" step=".01" min="0" class="form-control form-control-alternative" id="tituloLibro">
+                    </div>
+                    <div class="col-3">
+                      <label for="message-text" class="col-form-label">Cantidad:</label>
+                      <input type="number" step="any" min="0" class="form-control form-control-alternative" id="cantidadLibro">
                     </div>
                     <div class="col-6">
                       <label for="message-text" class="col-form-label">Categoria:</label>
@@ -716,13 +720,13 @@
               <form>
                 <div class="form-group">
                   <label for="recipient-name" class="col-form-label">ISBN:</label>
-                  <input type="text" class="form-control form-control-alternative" id="ISBN">
+                  <input type="text" class="form-control form-control-alternative" id="ISBN" value="9788441436008">
 
                   <label for="message-text" class="col-form-label">Autor:</label>
                   <div class="input-group mb-3">
                     <select class="custom-select form-control-alternative" id="inputGroupAutor">
-                      <option selected>Seleccionar...</option>
-                      <option value="1">Eduardo Galeano</option>
+                      <option>Seleccionar...</option>
+                      <option selected value="1">Eduardo Galeano</option>
                       <option value="2">Claudia Lars</option>
                       <option value="3">Jean Luke</option>
                     </select>
@@ -731,63 +735,63 @@
                   <label for="message-text" class="col-form-label">Editorial:</label>
                   <div class="input-group mb-3">
                     <select class="custom-select form-control-alternative" id="inputGroupEditorials">
-                      <option selected>Seleccionar...</option>
-                      <option value="1">Debolsillo</option>
+                      <option>Seleccionar...</option>
+                      <option selected value="1">Debolsillo</option>
                       <option value="2">Santillana El Salvador</option>
                       <option value="3">Editorial Planeta</option>
                     </select>
                   </div>
 
                   <label for="message-text" class="col-form-label">Titulo:</label>
-                  <input type="text" class="form-control form-control-alternative" id="tituloLibro">
+                  <input type="text" class="form-control form-control-alternative" id="tituloLibro" value="101 mensajes para decir te quiero para colorear y regalar">
 
                   <div class="row">
                     <div class="col-3">
                       <label for="message-text" class="col-form-label">Idioma:</label>
                       <div class="input group dropdown d-block">
                         <a href="#" class="btn btn-default dropdown-toggle " data-toggle="dropdown" id="navbarDropdownMenuLink2">
-                            <img src="../../resources/img/flags/SV.png" /> ...
+                          <img src="../../resources/img/flags/SV.png" /> ...
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink2">
-                            <li>
-                                <a class="dropdown-item" href="#">
-                                  <img src="../../resources/img/flags/DE.png" /> Deutsch
-                                </a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="#">
-                                  <img src="../../resources/img/flags/GB.png" /> English(UK)
-                                </a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="#">
-                                  <img src="../../resources/img/flags/FR.png" /> Français
-                                </a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="#">
-                                  <img src="../../resources/img/flags/ES.png" /> Español
-                                </a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="#">
-                                  <img src="../../resources/img/flags/PT.png" /> Portuges
-                                </a>
-                            </li>
+                          <li>
+                            <a class="dropdown-item" href="#">
+                              <img src="../../resources/img/flags/DE.png" /> Deutsch
+                            </a>
+                          </li>
+                          <li>
+                            <a class="dropdown-item" href="#">
+                              <img src="../../resources/img/flags/GB.png" /> English(UK)
+                            </a>
+                          </li>
+                          <li>
+                            <a class="dropdown-item" href="#">
+                              <img src="../../resources/img/flags/FR.png" /> Français
+                            </a>
+                          </li>
+                          <li>
+                            <a class="dropdown-item" href="#">
+                              <img src="../../resources/img/flags/ES.png" /> Español
+                            </a>
+                          </li>
+                          <li>
+                            <a class="dropdown-item" href="#">
+                              <img src="../../resources/img/flags/PT.png" /> Portuges
+                            </a>
+                          </li>
                         </ul>
                       </div>
                     </div>
                     <div class="col-4">
                       <label for="message-text" class="col-form-label">No. de páginas:</label>
-                      <input type="number" class="form-control form-control-alternative" id="noPags">
+                      <input type="number" class="form-control form-control-alternative" id="noPags" value="112">
                     </div>
                     <div class="col-5">
                       <label for="message-text" class="col-form-label">Ecuadernación:</label>
                       <div class="input-group mb-3">
                         <select class="custom-select form-control-alternative" id="inputGroupEcuadernacion">
-                          <option selected>Seleccionar...</option>
+                          <option>Seleccionar...</option>
                           <option value="1">Tapa Dura</option>
-                          <option value="2">Tapa Blanda</option>
+                          <option selected value="2">Tapa Blanda</option>
                           <option value="3">Tapa Dura de Bolsillo</option>
                           <option value="3">Tapa Blanda de Bolsillo</option>
                         </select>
@@ -796,22 +800,27 @@
                   </div>
 
                   <label for="message-text" class="col-form-label">Reseña:</label>
-                  <textarea id="resena" class="form-control form-control-alternative" rows="5" placeholder="Escriba una breve descripción ..."></textarea>
+                  <textarea id="resena" class="form-control form-control-alternative" rows="5" placeholder="Escriba una breve descripción ...">¡Prepara los lápices de colores! Di «Te quiero» de la forma más creativa y original. Medita sobre pensamientos positivos para crecer... ¡libera al artista que llevas dentro! Los libros para colorear son la última tendencia antiestrés para adultos y base para talleres terapéuticos. La arteterapia y el coloreado ayudan a relajarse, disminuir el estrés y a aumentar la concentración gracias a los lápices de colores.
+                  </textarea>
 
                   <div class="row">
-                    <div class="col-6">
+                    <div class="col-3">
                       <label for="message-text" class="col-form-label">Precio:</label>
-                      <input type="number" step=".01" min="0" class="form-control form-control-alternative" id="tituloLibro">
+                      <input type="number" step=".01" min="0" class="form-control form-control-alternative" id="tituloLibro" value="15.95">
+                    </div>
+                    <div class="col-3">
+                      <label for="message-text" class="col-form-label">Cantidad:</label>
+                      <input type="number" step="any" min="0" class="form-control form-control-alternative" id="cantidadLibro" value="150">
                     </div>
                     <div class="col-6">
                       <label for="message-text" class="col-form-label">Categoria:</label>
                       <div class="input-group mb-3">
                         <select class="custom-select form-control-alternative" id="inputGroupCategoria">
-                          <option selected>Seleccionar...</option>
+                          <option>Seleccionar...</option>
                           <option value="1">Comic</option>
                           <option value="2">Aventura</option>
                           <option value="3">Ciencia Ficción</option>
-                          <option value="3">Romance</option>
+                          <option selected value="3">Romance</option>
                         </select>
                       </div>
                     </div>
@@ -820,11 +829,11 @@
                   <div class="row">
                     <div class="col-6">
                       <label for="message-text" class="col-form-label">Me gusta:</label>
-                      <input type="number" class="form-control form-control-alternative" id="likes" disabled value="0">
+                      <input type="number" class="form-control form-control-alternative" id="likes" disabled value="5000">
                     </div>
                     <div class="col-6">
                       <label for="message-text" class="col-form-label">No me gusta:</label>
-                      <input type="number" class="form-control form-control-alternative" id="dislikes" disabled value="0">
+                      <input type="number" class="form-control form-control-alternative" id="dislikes" disabled value="755">
                     </div>
                   </div>
 
@@ -834,7 +843,7 @@
             </div>
             <div class="modal-footer">
               <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-              <button type="button" class="btn btn-warning" data-dismiss="modal" onclick="savedAlert('Libro')">Modificar</button>
+              <button type="button" class="btn btn-warning" data-dismiss="modal" onclick="modifyAlert('Libro')">Modificar</button>
             </div>
           </div>
         </div>
