645551c00e5da243272856a0e2319de5eca39de1
fabs1902 <fabiolamartinez190201@gmail.com>
2019-06-27 09:47:41 -0600

    Ejemplo de reporte, agregado

---
+++ core/reports/ejemplo.php
@@ -0,0 +1,9 @@
+<?php
+require('../../resources/libraries/fpdf.php');
+
+$pdf = new FPDF();
+$pdf->AddPage();
+$pdf->SetFont('Arial','B',16);
+$pdf->Cell(40,10,'¡Hola, Mundo!');
+$pdf->Output();
+?>
