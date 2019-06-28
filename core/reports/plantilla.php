3c507b51c20feaad7e0b22dbd8598f10612ae4f4
Steven <stevenbdf@gmail.com>
2019-06-27 22:25:20 -0600

    Optimizacion minima de la plantilla, cambio de color del header de la tabla

--- core/reports/plantilla.php
+++ core/reports/plantilla.php
@@ -5,41 +5,52 @@ require('../../core/helpers/validator.php');
 
 class PDF extends FPDF
 {
-private $titulo;
+	//Variable que almacena titulo del reporte
+	private $titulo;
 
-public function setTitulo($value){
-	$this->titulo= $value;
+	/**
+	 * Función que establece valor a titulo
+	 * @param {String} Titulo 
+	 */
+	public function setTitulo($value)
+	{
+		$this->titulo = $value;
+	}
+
+	// Cabecera de p�gina
+	function Header()
+	{
+		session_start();
+		ini_set('date.timezone', 'America/El_Salvador');
+		//Se especifica 10 mm de margenes, osea 1cm
+		$this->SetMargins(10, 10, 10);
+		//Se imprime el logo
+		$this->Image('../../resources/img/logo.png', 10, 10, 56);
+		//Definimos fuente, tipo, y tamaño para titulos
+		$this->SetFont('Arial', 'B', 20);
+		// Movernos a la derecha
+		$this->Cell(80);
+		//Se escribe el titulo del reporte, proviene de una variable
+		$this->Cell(115, 15, $this->titulo, 0, 0, 'R');
+		//Salto de linea
+		$this->Ln();
+		//Definimos la fuente, tipo, y tamaño para información de encabezado
+		$this->SetFont('Arial', 'I', 14);
+		//Información de en cabezado, autor y fecha del reporte, autor proviene de session
+		$this->Cell(65, 30, 'Autor: ' . $_SESSION['correoUsuario'], 0, 0, 'C');
+		$this->Cell(130, 30, 'Generado el: ' . date('d-m-Y H:i:s'), 0, 1, 'R');
+		//Salto de linea
+		$this->Ln(1);
+	}
+
+	//Pie de página
+	function Footer()
+	{
+		//Posición: a 1,5 cm del final
+		$this->SetY(-15);
+		//Arial italic 8
+		$this->SetFont('Arial', 'I', 8);
+		//Número de página
+		$this->Cell(0, 10, 'Pagina ' . $this->PageNo() . '/{nb}', 0, 0, 'C');
+	}
 }
-// Cabecera de p�gina
-function Header()
-{
-	// Logo
-	$this->Image('../../resources/img/logo.png',20,10,56);
-	// Arial bold 15
-	$this->SetFont('Arial','',25);
-	// Movernos a la derecha
-	$this->Cell(80);
-	// T�tulo
-	$this->Cell(100,15,$this->titulo,0,0,'R');
-
-	
-	
-	// Salto de l�nea
-	$this->Ln(10);
-	
-}
-
-// Pie de p�gina
-function Footer()
-{
-	// Posici�n: a 1,5 cm del final
-	$this->SetY(-15);
-	// Arial italic 8
-	$this->SetFont('Arial','I',8);
-	// N�mero de p�gina
-	$this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
-}
-}
-
-
-?>
