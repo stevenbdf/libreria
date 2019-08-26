c4810db42102b43409681211ae53b75fe8dd369c
fabs1902 <fabiolamartinez190201@gmail.com>
2019-08-25 20:28:27 -0600

    cierre de sesion con problemas

--- core/helpers/functions.js
+++ core/helpers/functions.js
@@ -84,3 +84,43 @@ $('#form-update-contrasena').submit(async () => {
     }
 })
 
+//creo la funcion usuario inactivo para cerrar la sesion del usuario
+function usuarioInactivo()
+{
+    //segundos cuando el usuario esta activo
+    var segundosActivo = 0;
+ 
+    //un minuto 60 x 1 = 60 segundos.
+    var maxSegundos = (60 * 1);
+ 
+    //metodo para ejecutar cada segundo(1000 milisegundos = 1 segundo)
+    setInterval(function(){
+        segundosActivo++;
+        //si el usuario esta inactivo por maxSegundos
+        if(segundosActivo > maxSegundos){
+            swal('El usuario ha estado inactivo por mas de ' + maxSegundos + ' segundos');
+            //Redirigir al logout
+            signOffCliente();
+        }
+    }, 1000);
+ 
+    //funcion para detectar que el usuario este activo
+    function activity()
+    {
+        //restablecer la variable de los segundo y volver a 0
+        segundosActivo = 0;
+    }
+    //Se define eventos DOM (detecta que el usario esta inactivo)
+   var activityEvents = [
+        'mousedown', 'mousemove', 'keydown',
+        'scroll', 'touchstart'
+   ];
+ 
+    //registra la actividad del usuario
+    activityEvents.forEach(function(eventName) {
+        document.addEventListener(eventName, activity, true);
+    });
+ 
+}
+
+usuarioInactivo();
